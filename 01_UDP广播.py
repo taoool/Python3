7b18b1a486a3b284bc26662508aa86b6469a8cde
taoool <30597887+taoool@users.noreply.github.com>
2020-03-16 11:34:42 +0800

    Update 01_UDP广播.py

--- 01_UDP广播.py
+++ 01_UDP广播.py
@@ -8,8 +8,7 @@ udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # socket.SO_BROADCAST  广播属性
 udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
 # 发送数据
-udp_socket.sendto("哈哈，打不过我把".encode("gbk"), ("192.168.217.255", 8080))
-# udp_socket.sendto("哈哈，打不过我把".encode("gbk"), ("255.255.255.255", 8080))
+udp_socket.sendto("这里是UDP广播".encode("gbk"), ("192.168.217.255", 8080))
 # 关闭套接字
 udp_socket.close()
 
